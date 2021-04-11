ba1404992a1b736da935d3104da020def05c80f1
Ulugbek Abdullayev <78225440+Ulugbek1024@users.noreply.github.com>
2021-04-11 12:41:26 -0700

    Add files via upload

---
+++ pythonCode/BuildingUpgrade.py
@@ -0,0 +1,51 @@
+import pyautogui, sys, time
+
+class UpgradeBuildings:
+    def TownHall(self):
+        buttonLocW1 = pyautogui.locateOnScreen('2_2_worker.png', confidence = 0.7)
+        if buttonLocW1 != None:
+            buttonLoc2 = pyautogui.locateOnScreen('TownHall.png', confidence = 0.7)
+            if buttonLoc2 != None:
+                buttonPoint = pyautogui.center(buttonLoc2)
+                buttonX, buttonY = buttonPoint
+                pyautogui.click(buttonX, buttonY)
+            else:
+                self.TownHall()
+            time.sleep(4)
+            buttonLoc3 = pyautogui.locateOnScreen('upgrade.png', confidence = 0.7)
+            if buttonLoc3 != None:
+                buttonPoint2 = pyautogui.center(buttonLoc3)
+                buttonX1, buttonY2 = buttonPoint2
+                pyautogui.click(buttonX1, buttonY2)
+            else:
+                self.TownHall()
+            time.sleep(3)
+            buttonLoc4 = pyautogui.locateOnScreen('twoK.png', confidence = 0.7)
+            if buttonLoc4 != None:
+                pyautogui.click()
+            else:
+                self.TownHall()
+        else:
+            pass
+    def GoldStorageUpgrade(self):
+        buttonLoc2 = pyautogui.locateOnScreen('goldStorage.png', confidence = 0.6)
+        if buttonLoc2 != None:
+            buttonPoint = pyautogui.center(buttonLoc2)
+            buttonX, buttonY = buttonPoint
+            pyautogui.click(buttonX, buttonY)
+        else:
+            self.GoldStorageUpgrade()
+        time.sleep(4)
+        buttonLoc3 = pyautogui.locateOnScreen('upgrade.png', confidence = 0.7)
+        if buttonLoc3 != None:
+            buttonPoint2 = pyautogui.center(buttonLoc3)
+            buttonX1, buttonY2 = buttonPoint2
+            pyautogui.click(buttonX1, buttonY2)
+        else:
+            self.GoldStorageUpgrade()
+        time.sleep(3)
+        buttonLoc4 = pyautogui.locateOnScreen('sevenFifty.png', confidence = 0.7)
+        if buttonLoc4 != None:
+            pyautogui.click()
+        else:
+            self.GoldStorageUpgrade()

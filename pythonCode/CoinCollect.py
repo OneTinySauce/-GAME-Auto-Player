50811630393bc678aeecf5639fa4add2ae2778e2
Ulugbek Abdullayev <78225440+Ulugbek1024@users.noreply.github.com>
2021-04-18 17:46:59 -0600

    Update CoinCollect.py

--- pythonCode/CoinCollect.py
+++ pythonCode/CoinCollect.py
@@ -2,13 +2,15 @@ import pyautogui, sys, time
 
 #Class for collecting accumulated coins
 class CoinCollectClass:
-    
-    #Function for collecting coins
     def collectCoin(self):
+        state = 'Failed'
         buttonLoc = pyautogui.locateOnScreen('coin.png', confidence = 0.7)
-        if buttonLoc != None:
-            buttonPoint = pyautogui.center(buttonLoc)
-            buttonX, buttonY = buttonPoint
-            pyautogui.click(buttonX, buttonY)
-        else:
-            self.collectCoin()
+        while state == 'Failed':
+            if buttonLoc != None:
+                buttonPoint = pyautogui.center(buttonLoc)
+                buttonX, buttonY = buttonPoint
+                pyautogui.click(buttonX, buttonY)
+            else:
+                self.collectCoin()
+            state = 'Passed'
+        return state

3b9d5668e7dd3632259528d3f812bb917e86fac4
Ulugbek Abdullayev <78225440+Ulugbek1024@users.noreply.github.com>
2021-04-18 17:48:00 -0600

    Update PlayerClass.py

--- pythonCode/PlayerClass.py
+++ pythonCode/PlayerClass.py
@@ -1,23 +1,29 @@
 import pyautogui, sys, time
-
-
 class PlayerInitiateClass:
 
     def WindowsClick(self):
         buttonLoc = pyautogui.locateOnScreen('windowsButton.png')
-        buttonPoint = pyautogui.center(buttonLoc)
-        buttonX, buttonY = buttonPoint
-        pyautogui.click(buttonX, buttonY)
+        if (buttonLoc != None):
+            buttonPoint = pyautogui.center(buttonLoc)
+            buttonX, buttonY = buttonPoint
+            pyautogui.click(buttonX, buttonY)
+            return 'Passed'
+        else:
+            return 'Failed'
 
     def NoxSearch(self):
-        pyautogui.write('Nox', interval=0.25)
-        pyautogui.press('enter')
+        (pyautogui.write('Nox', interval=0.25))
+        (pyautogui.press('enter'))
 
     def GameButton(self):
-        buttonLoc = pyautogui.locateOnScreen('ClashOfClansApp.png')
-        if buttonLoc != None:
-           buttonPoint = pyautogui.center(buttonLoc)
-           buttonX, buttonY = buttonPoint
-           pyautogui.click(buttonX, buttonY)
-        else:
-           self.GameButton()
+        state = 'Failed'
+        buttonLoc = pyautogui.locateOnScreen('ClashOfClansApp.png', confidence=0.7)
+        while state == 'Failed':
+            if buttonLoc != None:
+                buttonPoint = pyautogui.center(buttonLoc)
+                buttonX, buttonY = buttonPoint
+                pyautogui.click(buttonX, buttonY)
+            else:
+                self.GameButton()
+            state = 'Passed'
+        return state
